37c7cc1ccc90f801ee7ae2ab09d04db940dbebdb
Anton Lukashov <anton.lukashov@a-lukashov.do.local>
2021-11-23 21:26:08 +0200

    add Jenkinsfile and dockers

---
+++ HW_dev_pro/jenkins/jenkins-data/aws-s3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DATE=$(date +%H-%M-%S)
+BACKUP=db-$DATE.sql
+
+DB_HOST=$1
+DB_PASSWD=$2
+DB_NAME=$3
+AWS_SECRET=$4
+BUCKET_NAME=$5
+
+mysqldump -u root -h $DB_HOST -p$DB_PASSWD $DB_NAME > /tmp/$BACKUP  && \
+export AWS_ACCESS_KEY_ID=AKIAZ4LWFEKHIIRZQL6F && \
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET && \
+echo "Uploading your $BACKUP backup" && \
+#AWS_ACCESS_KEY_ID=AKIAZ4LWFEKHIIRZQL6F AWS_SECRET_ACCESS_KEY=$AWS_SECRET 
+aws s3 cp /tmp/db-$DATE.sql s3://$BUCKET_NAME/$BACKUP
+
