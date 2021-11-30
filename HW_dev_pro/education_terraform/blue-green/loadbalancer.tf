32b5920af736ab47439ba6d0151a18e94bfe73d2
Anton Lukashov <anton.lukashov@a-lukashov.do.local>
2021-11-30 10:03:03 +0200

    add python script and blue-green deploy_2

--- HW_dev_pro/education_terraform/blue-green/loadbalancer.tf
+++ HW_dev_pro/education_terraform/blue-green/loadbalancer.tf
@@ -1,40 +1,35 @@
-resource "cloudflare_record" "set-lb-cname" {
-  zone_id                     = var.zone_id
-  name                        = "@"
-  value                       = aws_lb.alb.dns_name
-  type                        = "CNAME"
-  ttl                         = "1"
-  proxied                     = "true"
-}
-
-resource "aws_lb" "alb" {
-  name                        = "alb"
+resource "aws_lb" "albGreen" {
+  name                        = "albGreen"
   internal                    = false
   load_balancer_type          = "application"
   security_groups             = [aws_security_group.ssh-http-allowed.id]
   subnets                     = [aws_subnet.public_1a.id, aws_subnet.public_1b.id]
 
   enable_deletion_protection  = false
+
+  tags = {
+    Name                      = "albGreen"
+  }
 }
 
-resource "aws_lb_listener" "appserver" {
-  load_balancer_arn           = aws_lb.alb.arn
+resource "aws_lb_listener" "appserver_green" {
+  load_balancer_arn           = aws_lb.albGreen.arn
   port                        = "80"
   protocol                    = "HTTP"
 
   default_action {
     type                      = "forward"
-    target_group_arn          = aws_lb_target_group.application.arn
+    target_group_arn          = aws_lb_target_group.application_green.arn
   }
 }
 
-resource "aws_lb_listener_rule" "phpmyadmin" {
-  listener_arn                = aws_lb_listener.appserver.arn
+resource "aws_lb_listener_rule" "phpmyadmin_green" {
+  listener_arn                = aws_lb_listener.appserver_green.arn
   priority                    = 100
 
   action {
     type                      = "forward"
-    target_group_arn          = aws_lb_target_group.myadmin.arn
+    target_group_arn          = aws_lb_target_group.myadmin_green.arn
   }
   condition {
     path_pattern {
@@ -45,30 +40,30 @@ resource "aws_lb_listener_rule" "phpmyadmin" {
 
 
 # app_web_server target group
-resource "aws_lb_target_group" "application" {
-  name                        = "nginx"
+resource "aws_lb_target_group" "application_green" {
+  name                        = "nginxGreen"
   port                        = 80
   protocol                    = "HTTP"
   target_type                 = "instance"
   vpc_id                      = aws_vpc.vpc_for_dev_pro.id
 }
 
-resource "aws_lb_target_group_attachment" "nginx1" {
-  target_group_arn            = aws_lb_target_group.application.arn
-  target_id                   = aws_instance.app_server.id
+resource "aws_lb_target_group_attachment" "nginx1_green" {
+  target_group_arn            = aws_lb_target_group.application_green.arn
+  target_id                   = aws_instance.app_server_2_green.id
   port                        = 80
 }
 
-resource "aws_lb_target_group_attachment" "nginx2" {
-  target_group_arn            = aws_lb_target_group.application.arn
-  target_id                   = aws_instance.app_server_2.id
+resource "aws_lb_target_group_attachment" "nginx2_green" {
+  target_group_arn            = aws_lb_target_group.application_green.arn
+  target_id                   = aws_instance.app_server_2_green.id
   port                        = 80
 }
 
 
 # myadmin target group
-resource "aws_lb_target_group" "myadmin" {
-  name                        = "myadmin"
+resource "aws_lb_target_group" "myadmin_green" {
+  name                        = "myadminGreen"
   port                        = 80
   protocol                    = "HTTP"
   target_type                 = "instance"
@@ -76,7 +71,102 @@ resource "aws_lb_target_group" "myadmin" {
 }
 
 resource "aws_lb_target_group_attachment" "myadmin" {
-  target_group_arn            = aws_lb_target_group.myadmin.arn
-  target_id                   = aws_instance.myadmin.id
+  target_group_arn            = aws_lb_target_group.myadmin_green.arn
+  target_id                   = aws_instance.myadmin_green.id
   port                        = 80
 }
+
+
+##################################################
+#################   BLUE   #######################
+##################################################
+
+resource "aws_lb" "albBlue" {
+  name                        = "albBlue"
+  internal                    = false
+  load_balancer_type          = "application"
+  security_groups             = [aws_security_group.ssh-http-allowed.id]
+  subnets                     = [aws_subnet.public_1a.id, aws_subnet.public_1b.id]
+
+  enable_deletion_protection  = false
+
+  tags = {
+    Name                      = "albBlue"
+  }
+}
+
+
+resource "aws_lb_listener" "appserver_blue" {
+  load_balancer_arn           = aws_lb.albBlue.arn
+  port                        = "80"
+  protocol                    = "HTTP"
+
+  default_action {
+    type                      = "forward"
+    target_group_arn          = aws_lb_target_group.application_blue.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "phpmyadmin_blue" {
+  listener_arn                = aws_lb_listener.appserver_blue.arn
+  priority                    = 100
+
+  action {
+    type                      = "forward"
+    target_group_arn          = aws_lb_target_group.myadmin_blue.arn
+  }
+  condition {
+    path_pattern {
+      values                  = ["/phpMyAdmin/*"]
+    }
+  }
+}
+
+# app_web_server target group
+resource "aws_lb_target_group" "application_blue" {
+  name                        = "nginxBlue"
+  port                        = 80
+  protocol                    = "HTTP"
+  target_type                 = "instance"
+  vpc_id                      = aws_vpc.vpc_for_dev_pro.id
+}
+
+resource "aws_lb_target_group_attachment" "nginx1_blue" {
+  target_group_arn            = aws_lb_target_group.application_blue.arn
+  target_id                   = aws_instance.app_server_blue.id
+  port                        = 80
+}
+
+resource "aws_lb_target_group_attachment" "nginx2_blue" {
+  target_group_arn            = aws_lb_target_group.application_blue.arn
+  target_id                   = aws_instance.app_server_2_blue.id
+  port                        = 80
+}
+
+# myadmin target group
+resource "aws_lb_target_group" "myadmin_blue" {
+  name                        = "myadminBlue"
+  port                        = 80
+  protocol                    = "HTTP"
+  target_type                 = "instance"
+  vpc_id                      = aws_vpc.vpc_for_dev_pro.id
+}
+
+resource "aws_lb_target_group_attachment" "myadmin_blue_2" {
+  target_group_arn            = aws_lb_target_group.myadmin_blue.arn
+  target_id                   = aws_instance.myadmin_blue.id
+  port                        = 80
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- HW_dev_pro/education_terraform/blue-green/main.tf
+++ HW_dev_pro/education_terraform/blue-green/main.tf
@@ -4,11 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.27"
     }
-cloudflare = {
-      source = "cloudflare/cloudflare"
-      version = "~> 3.0"
-    }
-
   }
 
   required_version = ">= 0.14.9"
@@ -20,50 +15,40 @@ provider "aws" {
   region          = var.region
 }
 
-provider "cloudflare" {
-  email           = var.email
-  api_key         = var.cloudflare_api_key
-}
+##############################
+#   GREEN   #
+##############################
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "app_server_green" {
   ami                         = var.image_id
   instance_type               = var.instance_type
-#  user_data                   = file("user_data.sh")
   subnet_id                   = "${aws_subnet.public_1a.id}"
   vpc_security_group_ids      = ["${aws_security_group.ssh-http-allowed.id}"]
   key_name                    = var.key_name
-#  associate_public_ip_address = true
-#  public_dns                  = true
 
   tags = {
     Name                      = "app_server"
   }
 }
 
-resource "aws_instance" "app_server_2" {
+resource "aws_instance" "app_server_2_green" {
   ami                         = var.image_id
   instance_type               = var.instance_type
-#  user_data                   = file("user_data.sh")
   subnet_id                   = "${aws_subnet.public_1a.id}"
   vpc_security_group_ids      = ["${aws_security_group.ssh-http-allowed.id}"]
   key_name                    = var.key_name
-#  associate_public_ip_address = true
-#  public_dns                  = true
 
   tags = {
     Name                      = "app_server"
   }
 }
 
-resource "aws_instance" "myadmin" {
+resource "aws_instance" "myadmin_green" {
   ami                         = var.image_id
   instance_type               = var.instance_type
-#  user_data                   = file("bootstrap_for_back.sh")
   subnet_id                   = "${aws_subnet.public_1b.id}"
   vpc_security_group_ids      = ["${aws_security_group.ssh-http-allowed.id}"]
   key_name                    = var.key_name
-#  associate_public_ip_address = true
-#  public_dns                  = true
 
   tags = {
     Name                      = "myadmin"
@@ -71,20 +56,43 @@ resource "aws_instance" "myadmin" {
 }
 
 
-#resource "aws_instance" "bastion" {
-#  ami                         = var.image_id
-#  instance_type               = var.instance_type
-#  user_data                   = file("scripts_for_bastion.sh")
-#  subnet_id                   = "${aws_subnet.public_1b.id}"
-#  vpc_security_group_ids      = ["${aws_security_group.ssh-http-allowed.id}"]
-#  key_name                    = var.key_name
-#  associate_public_ip_address = true
-#
-#  tags = {
-#    Name                      = "bastion"
-#  }
-#}
+##############################
+#   BLUE   #
+##############################
 
-output "load_balancer_name" {
-  value = aws_lb.alb.dns_name
+resource "aws_instance" "app_server_blue" {
+  ami                         = var.image_id
+  instance_type               = var.instance_type
+  subnet_id                   = "${aws_subnet.public_1a.id}"
+  vpc_security_group_ids      = ["${aws_security_group.ssh-http-allowed.id}"]
+  key_name                    = var.key_name
+
+  tags = {
+    Name                      = "app_server"
+  }
 }
+
+resource "aws_instance" "app_server_2_blue" {
+  ami                         = var.image_id
+  instance_type               = var.instance_type
+  subnet_id                   = "${aws_subnet.public_1a.id}"
+  vpc_security_group_ids      = ["${aws_security_group.ssh-http-allowed.id}"]
+  key_name                    = var.key_name
+
+  tags = {
+    Name                      = "app_server"
+  }
+}
+
+resource "aws_instance" "myadmin_blue" {
+  ami                         = var.image_id
+  instance_type               = var.instance_type
+  subnet_id                   = "${aws_subnet.public_1b.id}"
+  vpc_security_group_ids      = ["${aws_security_group.ssh-http-allowed.id}"]
+  key_name                    = var.key_name
+
+  tags = {
+    Name                      = "myadmin"
+  }
+}
+
