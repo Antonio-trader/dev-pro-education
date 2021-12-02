a2330698a3ca374c2cb228b2fe8b991d4983807b
Anton Lukashov <anton.lukashov@a-lukashov.do.local>
2021-12-02 20:15:31 +0200

    compleate

--- HW_dev_pro/education_terraform/HW_terraform_code_without_modules/security.tf
+++ HW_dev_pro/education_terraform/HW_terraform_code_without_modules/security.tf
@@ -28,6 +28,13 @@ resource "aws_security_group" "ssh-http-allowed" {
     to_port           = 80
     cidr_blocks       = ["0.0.0.0/0"]
   }
+
+    ingress {
+    from_port         = 3306
+    protocol          = "tcp"
+    to_port           = 3306
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
   tags = {
     Name = "Security group for myAdmin"
   }
