41d5be2e0571020011ce56b42b9cbd2a94240fe0
Anton Lukashov <anton.lukashov@a-lukashov.do.local>
2021-12-02 19:17:15 +0200

    add python script to repo

---
+++ HW_dev_pro/education_ansible/roles/install_phpmyadmin/templates/set_db_to_phpmyadmin.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import subprocess
+import argparse
+import json
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-ec2', '--aws_ec2', help='Set path to aws_ec2.yml', required=True)
+    parser.add_argument('-sample', '--sample', help='Path to sample config for phpMyAdmin', required=True)
+    parser.add_argument('-config', '--config', help='Path to config file for phpMyAdmin', required=True)
+
+    return parser.parse_args()
+
+
+def get_aws_ec2_buffer(path_to_aws_ec2):
+    """ Get all balancers """
+    output = subprocess.run(["ansible-inventory", "-i", f"{path_to_aws_ec2}", "--list"], capture_output=True, text=True,)
+    return output.stdout
+
+
+def parse_buffer(buffer):
+    with open("test_error.txt", "w") as w:
+        w.write(buffer)
+
+    my_dictionary = json.loads(buffer)
+    if my_dictionary:
+        for tag in my_dictionary:
+            if "db" in tag:
+                return my_dictionary[tag].get("hosts"), my_dictionary
+
+
+def get_db_ip(tag, buffer_dict):
+    if tag and buffer_dict:
+        if isinstance(tag, list):
+            db_host = tag[0]
+            for _ in buffer_dict:
+                for var in buffer_dict.get("_meta").get("hostvars"):
+                    if db_host == var:
+                        return buffer_dict.get("_meta").get("hostvars").get(var).get("private_ip_address")
+
+
+def make_config_file(path_to_sample_conf, private_ip, path_for_save_config):
+    with open(path_to_sample_conf) as r:
+        text = r.read().replace("localhost", private_ip)
+    with open(path_for_save_config, "w") as w:
+        w.write(text)
+
+
+def main():
+    args = parse_arguments()
+
+    buffer = get_aws_ec2_buffer(args.aws_ec2)
+    tag, buffer_dict = parse_buffer(buffer)
+    private_ip = get_db_ip(tag=tag, buffer_dict=buffer_dict)
+    make_config_file(
+        path_to_sample_conf=args.sample,
+        path_for_save_config=args.config,
+        private_ip=private_ip
+    )
+    print(private_ip)
+
+
+if __name__ == "__main__":
+    main()
+
