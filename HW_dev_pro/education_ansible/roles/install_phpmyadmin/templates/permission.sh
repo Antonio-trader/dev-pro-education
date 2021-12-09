f712676a82192badf6a7a16379cac02f186ca7aa
Anton Lukashov <anton.lukashov@a-lukashov.do.local>
2021-12-09 18:10:28 +0200

    new code without python

--- HW_dev_pro/education_ansible/roles/install_phpmyadmin/templates/permission.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# add permissions
-groupadd www
-usermod -a -G www ec2-user
-chown -R ec2-user:www /usr/share/nginx/html
-chmod 2775 /usr/share/nginx/html && find /usr/share/nginx/html -type d -exec  chmod 2775 {} \;
-find /usr/share/nginx/html -type f -exec sudo chmod 0664 {} \;
